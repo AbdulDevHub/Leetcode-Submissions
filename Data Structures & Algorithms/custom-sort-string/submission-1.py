fb8f8b067c964fd2fd10c4f2cf0a235c3229f967
Abdul Hadi Khan <ahkn63@gmail.com>
2026-06-29 13:15:57 -0400

    Add: custom-sort-string - submission-1

---
+++ Data Structures & Algorithms/custom-sort-string/submission-1.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        count = Counter(s)
+        res = []
+        for char in order:
+            if char in count:
+                res.append(char * count[char])
+                del count[char]
+        for char, val in count.items(): res.append(char * val)
+        return "".join(res)
