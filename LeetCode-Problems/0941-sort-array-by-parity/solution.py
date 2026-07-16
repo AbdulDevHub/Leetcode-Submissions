458a491972bd47b7d2e12da5aac53158ef4ad27e
Abdul Hadi Khan <ahkn63@gmail.com>
2026-07-16 16:07:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.8 MB (27.23%)

---
+++ LeetCode-Problems/0941-sort-array-by-parity/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            while left < right and nums[left] % 2 == 0: left += 1 # Look for even
+            while left < right and nums[right] % 2 != 0: right -= 1 # Look for odd
+            if left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+        return nums
