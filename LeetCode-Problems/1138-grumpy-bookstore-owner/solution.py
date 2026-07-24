b72f6568c1ada9c73066bee3dab67839fb59399e
Abdul Hadi Khan <ahkn63@gmail.com>
2026-07-24 16:35:18 +0000

    Sync LeetCode submission Runtime - 15 ms (72.41%), Memory - 21.7 MB (84.37%)

---
+++ LeetCode-Problems/1138-grumpy-bookstore-owner/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+        left = 0
+        window = maxWindow = 0
+        satisfied = 0
+
+        for right in range(len(customers)):
+            if grumpy[right]: window += customers[right]
+            else: satisfied += customers[right]
+
+            if right - left + 1 > minutes:
+                if grumpy[left]: window -= customers[left]
+                left += 1
+            maxWindow = max(window, maxWindow)
+        return satisfied + maxWindow
