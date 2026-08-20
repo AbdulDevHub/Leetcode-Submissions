fd3a8d5c746fe5a047d908ea411bf85607d5fddc
Abdul Hadi Khan <ahkn63@gmail.com>
2026-08-20 17:05:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (56.85%)

---
+++ LeetCode-Problems/0069-sqrtx/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        r = x
+        while r * r > x:
+            r = (r + x // r) // 2
+        return r
+
+
+# Newton's Method (Integer Newton-Raphson): 
